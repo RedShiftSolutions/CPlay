1d379d5e0d9a880f57c545d3fcd38cf87b72ba69
Brad Brown <brad.brown@red-shift.ca>
2022-09-16 22:19:01 -0400

    Fording an error in source

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!  It's funky outside!");
+        and here is something to force an error
 	}
 }
