730ad3dea61727f1e33610adcc9d8851e18fad74
Brad Brown <brad.brown@red-shift.ca>
2022-09-16 22:20:21 -0400

    Fixed source error

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!  It's funky outside!");
-        and here is something to force an error
 	}
 }
